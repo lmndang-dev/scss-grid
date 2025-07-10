0e77c3672650cd3f266c5aa7d89364eb75d894df
Le Minh Nhat Dang <leminhnhat.dang@Les-MacBook-Pro.local>
2025-07-10 02:39:31 -0400

    Learning SCSS Grid

---
+++ src/components/BasicGrid.jsx
@@ -0,0 +1,16 @@
+import "./BasicGrid.styles.scss";
+
+const BasicGird = () => {
+  return (
+    <div className="container">
+      <div className="item item--1">1: Orange</div>
+      <div className="item item--2">2: Green</div>
+      <div className="item item--3">3: Violet</div>
+      <div className="item item--4">4: Pink</div>
+      <div className="item item--5">5: Blue</div>
+      <div className="item item--6">6: Brown</div>
+    </div>
+  );
+};
+
+export default BasicGird;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import "./Layout.styles.scss";
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <div className="header">Header</div>
+      <div className="box-1">Small Box 1</div>
+      <div className="box-2">Small Box 2</div>
+      <div className="box-3">Small Box 3</div>
+      <div className="sidebar">Sidebar</div>
+      <div className="main">Main Content</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+};
+
+export default Layout;
